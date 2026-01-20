3e8cad3c792bb382eb70bd96f89d343db43bdf64
seshukumar0235 <132326918+seshukumar0235@users.noreply.github.com>
2026-01-20 12:14:15 +0530

    Create init.database.sql

---
+++ Scripts/init.database.sql
@@ -0,0 +1,25 @@
+-- create Database datawarehouse
+
+use master
+Go
+
+-- creating a database, check if it is avaliable or not
+create database dataWarehouse;
+Go 
+
+use dataWarehouse;
+Go
+
+-- " create a different Schemas for diff layers using meddilion Archectiure "
+
+-- bronze layer used for sorce data layer
+create schema bronze;
+Go
+
+-- silver layer used for Transfomations 
+create schema silver;
+Go
+
+-- gold layer used for reporting and analysis
+ create schema gold;
+Go
